de732ab457c3448f906721cc1722d9d795d12cd1
suwings <suwings@outlook.com>
2019-03-16 23:07:01 +0800

    Up gghcdepais

--- mine/PapaProject/PythonGet/pyquery_replite.py
+++ mine/PapaProject/PythonGet/pyquery_replite.py
@@ -8,6 +8,7 @@ import requests
 
 
 def init_reptile(tar_url):
+    """ 初始化爬虫 """
     web_res_context = requests.get(tar_url)
     web_res_context.encoding = 'utf-8'
     document = pquery(web_res_context.text)
@@ -19,18 +20,22 @@ def init_reptile(tar_url):
 
 
 def comp_http_url(base_url, tar_url):
+    """ http url 整理与规范 """
     base_url_obj = urlparse(base_url)
     last_url = ""
     if tar_url[:4] in 'http':
         last_url = tar_url
     else:
-        last_url= base_url_obj.scheme + "://" + base_url_obj.netloc + os.path.normpath(os.path.join(
-                os.path.dirname(base_url_obj.path), tar_url)).replace("\\", "/")
+        last_url = base_url_obj.scheme + "://" + base_url_obj.netloc + os.path.normpath(os.path.join(
+            os.path.dirname(base_url_obj.path), tar_url)).replace("\\", "/")
     return last_url
 
 
-def reptile_resurgence_links(tar_url, max_layer, max_container="",res_links=[],next_url=""):
-    """爬虫层次挖掘，对目标 URL 进行多层挖链接"""
+def reptile_resurgence_links(tar_url, max_layer, max_container="", a_elem="a", res_links=[], next_url=""):
+    """
+    爬虫层次挖掘，对目标 URL 进行多层挖链接
+    参数：目标 URL | 最大层数 | 爬取范围 | 内部使用，返回列表 | 内部使用 下一个目标
+    """
     if next_url != "" and next_url[:4] in 'http':
         res_links.append(next_url)
     if max_layer <= 0:
@@ -39,19 +44,31 @@ def reptile_resurgence_links(tar_url, max_layer, max_container="",res_links=[],n
     document = rep['document']
     if max_container == "":
         # 无差别对网页爬取
-        all_tag = document.find('a').items()
+        all_tag = document.find(a_elem).items()
         for tag in all_tag:
-            reptile_resurgence_links(tar_url, max_layer - 1, "",res_links,comp_http_url(tar_url,tag.attr('href')))
+            reptile_resurgence_links(
+                tar_url, max_layer - 1,
+                max_container="",
+                a_elem=a_elem,
+                res_links=res_links,
+                next_url=comp_http_url(tar_url, tag.attr('href'))
+            )
     else:
         # 专注于某一区域对网页爬虫 推荐这种方法只爬一层
         container_tags = document.find(max_container).items()
         for tag1 in container_tags:
-            children_tags = tag1.find('a').items()
+            children_tags = tag1.children(a_elem).items()
             for tag2 in children_tags:
-                reptile_resurgence_links(tar_url, max_layer - 1, max_container, res_links,comp_http_url(tar_url,tag2.attr('href')))
+                # 可以在这里增加 callback 有效减少请求次数
+                reptile_resurgence_links(
+                    tar_url, max_layer - 1,
+                    max_container=max_container,
+                    res_links=res_links,
+                    next_url=comp_http_url(tar_url, tag2.attr('href'))
+                )
     # 爬取之后将会获得每一个链接
     return res_links
-    
+
 
 def get_context_website(reptile, configs):
     """
@@ -106,35 +123,6 @@ def get_one_webstie(reptile, mainElem, linkElem, TimeElem, titleElem=None):
         results.append(tmps)
     return results
 
-print(reptile_resurgence_links("http://www.miit.gov.cn/n1146290/n1146392/index.html",1,".clist_con"))
 
-# get_context_website('http://www.moj.gov.cn/news/content/2019-03/15/zfyw_230595.html', {
-#     "title": ".con_bt",
-#     "context": "#content"
-# })
-
-# get_context_website(init_reptile('http://www.miit.gov.cn/n1146290/n1146392/c6669125/content.html'), {
-#     "title": "#con_title",
-#     "context": "div#con_con"
-# })
-
-
-# news_center = []
-
-# news_center += get_one_webstie(init_reptile("http://www.gov.cn/zhengce/zuixin.htm"),
-#                                ".news_box>.list h4", 'a', 'span.date')
-
-# news_center += get_one_webstie(init_reptile("http://www.miit.gov.cn/n1146295/n1652858/n1653018/index.html"),
-                            #    ".clist_con li", 'a', 'span>a')
-# news_center += get_one_webstie("http://www.mohrss.gov.cn/gkml/zcjd/index.html",
-#                                "#documentContainer>.row", '.mc a', '.fbrq>font',
-#                                )
-# news_center += get_one_webstie("http://www.moj.gov.cn/news/node_zfyw.html",
-#                                "ul.font_black_16>li", 'dt>a', 'dd',)
-
-
-# news_count=1
-# for news in news_center:
-#     print(str(news_count) + "." + news['title'] +
-#           "\n 时间 "+news['time']+" | 链接：" + news['href'])
-#     news_count += 1
+print(reptile_resurgence_links(
+    "http://www.miit.gov.cn/n1146290/n1146392/index.html", 1, ".clist_con", "ul>li>a"))
