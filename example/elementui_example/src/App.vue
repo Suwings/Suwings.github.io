dc0282b09c37250273b7fb3b03851234ac9a09c7
Suwings <Suwings@outlook.com>
2020-12-15 20:11:22 +0800

    新增 初始化 Element-ui 项目

---
+++ example/elementui_example/src/App.vue
@@ -0,0 +1,42 @@
+<!--
+ * @Author: Copyright(c) 2020 Suwings
+ * @Date: 2020-12-15 20:04:09
+ * @LastEditTime: 2020-12-15 20:11:00
+ * @Description: 
+-->
+<template>
+  <div id="app">
+    <img src="./assets/logo.png" />
+    <div>
+      <p>
+        If Element is successfully added to this project, you'll see an
+        <el-button type="primary">主要按钮</el-button>
+        below
+      </p>
+      <el-button>el-button</el-button>
+    </div>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "app",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
