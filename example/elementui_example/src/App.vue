c40039e141a355f828ec3423b1b467d546fc4289
Suwings <Suwings@outlook.com>
2020-12-15 21:49:40 +0800

    Element 原型

--- example/elementui_example/src/App.vue
+++ example/elementui_example/src/App.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: Copyright(c) 2020 Suwings
  * @Date: 2020-12-15 20:04:09
- * @LastEditTime: 2020-12-15 20:55:52
+ * @LastEditTime: 2020-12-15 21:49:13
  * @Description: 
 -->
 <template>
@@ -9,63 +9,112 @@
     <el-aside width="250px">
       <!-- <el-scrollbar style="height: 100%; width: 250px"> -->
       <el-menu
-        :default-openeds="['1', '3']"
+        :default-openeds="['1', '2']"
         background-color="#333333"
         text-color="#FFFFFF"
         style="height: 100%"
       >
+        <el-menu-item index="999" style="text-align: center">
+          <img src="./assets/logo.png" alt="" srcset="" />
+        </el-menu-item>
+
         <el-submenu index="1">
-          <template slot="title"
-            ><i class="el-icon-message"></i>导航一</template
-          >
-          <el-menu-item-group>
-            <template slot="title">分组一</template>
-            <el-menu-item index="1-1">选项1</el-menu-item>
-            <el-menu-item index="1-2">选项2</el-menu-item>
-          </el-menu-item-group>
-          <el-menu-item-group title="分组2">
-            <el-menu-item index="1-3">选项3</el-menu-item>
-          </el-menu-item-group>
-          <el-submenu index="1-4">
-            <template slot="title">选项4</template>
-            <el-menu-item index="1-4-1">选项4-1</el-menu-item>
-          </el-submenu>
+          <template slot="title">
+            <i class="el-icon-message"></i>服务管理
+          </template>
+          <el-menu-item index="1-1">面板总览</el-menu-item>
+          <el-menu-item index="1-2">应用实例</el-menu-item>
+          <el-menu-item index="1-3">用户管理</el-menu-item>
         </el-submenu>
         <el-submenu index="2">
-          <template slot="title"><i class="el-icon-menu"></i>导航二</template>
-          <el-menu-item-group>
-            <template slot="title">分组一</template>
-            <el-menu-item index="2-1">选项1</el-menu-item>
-            <el-menu-item index="2-2">选项2</el-menu-item>
-          </el-menu-item-group>
-          <el-menu-item-group title="分组2">
-            <el-menu-item index="2-3">选项3</el-menu-item>
-          </el-menu-item-group>
-          <el-submenu index="2-4">
-            <template slot="title">选项4</template>
-            <el-menu-item index="2-4-1">选项4-1</el-menu-item>
-          </el-submenu>
+          <template slot="title">
+            <i class="el-icon-message"></i>核心功能
+          </template>
+          <el-menu-item index="2-1">守护进程</el-menu-item>
+          <el-menu-item index="2-2">容器</el-menu-item>
+          <el-menu-item index="2-3">镜像</el-menu-item>
         </el-submenu>
         <el-submenu index="3">
-          <template slot="title"
-            ><i class="el-icon-setting"></i>导航三</template
-          >
-          <el-menu-item-group>
-            <template slot="title">分组一</template>
-            <el-menu-item index="3-1">选项1</el-menu-item>
-            <el-menu-item index="3-2">选项2</el-menu-item>
-          </el-menu-item-group>
-          <el-menu-item-group title="分组2">
-            <el-menu-item index="3-3">选项3</el-menu-item>
-          </el-menu-item-group>
-          <el-submenu index="3-4">
-            <template slot="title">选项4</template>
-            <el-menu-item index="3-4-1">选项4-1</el-menu-item>
-          </el-submenu>
+          <template slot="title">
+            <i class="el-icon-message"></i>扩展功能
+          </template>
+          <el-menu-item index="3-1">应用程序接口</el-menu-item>
+          <el-menu-item index="3-2">集群</el-menu-item>
+          <el-menu-item index="3-3">设置</el-menu-item>
         </el-submenu>
       </el-menu>
       <!-- </el-scrollbar> -->
     </el-aside>
+    <el-container>
+      <el-header>
+        <el-menu class="el-menu-demo" mode="horizontal">
+          <el-menu-item index="1">处理中心</el-menu-item>
+          <el-submenu index="2">
+            <template slot="title">我的工作台</template>
+            <el-menu-item index="2-1">选项1</el-menu-item>
+            <el-menu-item index="2-2">选项2</el-menu-item>
+            <el-menu-item index="2-3">选项3</el-menu-item>
+            <el-submenu index="2-4">
+              <template slot="title">选项4</template>
+              <el-menu-item index="2-4-1">选项1</el-menu-item>
+              <el-menu-item index="2-4-2">选项2</el-menu-item>
+              <el-menu-item index="2-4-3">选项3</el-menu-item>
+            </el-submenu>
+          </el-submenu>
+          <el-menu-item index="3" disabled>消息中心</el-menu-item>
+          <el-menu-item index="4"
+            ><a href="https://www.ele.me" target="_blank"
+              >订单管理</a
+            ></el-menu-item
+          >
+        </el-menu>
+      </el-header>
+
+      <el-scrollbar style="height: 110%">
+        <el-main>
+          <el-card class="box-card">
+            <div v-for="o in 4" :key="o" class="text item">
+              {{ "列表内容 " + o }}
+            </div>
+          </el-card>
+          <el-card class="box-card">
+            <div v-for="o in 4" :key="o" class="text item">
+              {{ "列表内容 " + o }}
+            </div>
+          </el-card>
+          <el-card class="box-card">
+            <div v-for="o in 4" :key="o" class="text item">
+              {{ "列表内容 " + o }}
+            </div>
+          </el-card>
+          <el-card class="box-card">
+            <div v-for="o in 4" :key="o" class="text item">
+              {{ "列表内容 " + o }}
+            </div>
+          </el-card>
+          <el-card class="box-card">
+            <div v-for="o in 4" :key="o" class="text item">
+              {{ "列表内容 " + o }}
+            </div>
+          </el-card>
+          <el-card class="box-card">
+            <div v-for="o in 4" :key="o" class="text item">
+              {{ "列表内容 " + o }}
+            </div>
+          </el-card>
+          <el-card class="box-card">
+            <div v-for="o in 4" :key="o" class="text item">
+              {{ "列表内容 " + o }}
+            </div>
+          </el-card>
+          <el-card class="box-card">
+            <div v-for="o in 4" :key="o" class="text item">
+              {{ "列表内容 " + o }}
+            </div>
+          </el-card>
+        </el-main>
+      </el-scrollbar>
+    </el-container>
   </el-container>
 </template>
 
