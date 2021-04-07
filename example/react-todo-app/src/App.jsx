a22fedffd5a9dd00400c935672deef220d188f1d
Suwings <Suwings@outlook.com>
2021-04-07 17:03:14 +0800

    创建 项目结构

---
+++ example/react-todo-app/src/App.jsx
@@ -0,0 +1,18 @@
+/*
+ * @Author: Copyright(c) 2020 Suwings
+ * @Date: 2021-04-07 16:34:51
+ * @LastEditTime: 2021-04-07 16:57:12
+ * @Description: 
+ */
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h4>TO DO 列表</h4>
+    </div>
+  );
+}
+
+export default App;
---
+++ example/react-todo-app/src/Lists.jsx
@@ -0,0 +1,18 @@
+/*
+ * @Author: Copyright(c) 2020 Suwings
+ * @Date: 2021-04-07 16:34:51
+ * @LastEditTime: 2021-04-07 17:00:29
+ * @Description: 
+ */
+
+
+function Lists() {
+  return (
+    <div className="Lists">
+      <p>测试</p>
+      <ul></ul>
+    </div>
+  );
+}
+
+export default Lists;
